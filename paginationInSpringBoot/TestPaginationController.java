13fa868225b79908cc763f0f45b56c8ed580360b
Abhishek <kumarabhishek0388@gmail.com>
2020-06-25 17:12:02 +0530

    renaming file

--- paginationInSpringBoot/TestPaginationController.java
+++ paginationInSpringBoot/TestPaginationController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class TestPagination {
+public class TestPaginationController {
 
     @Autowired
     private CaseService caseService;
