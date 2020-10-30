1d4383a8410c56bdbc337262378617fb3d5b11e4
Abhishek <kumarabhishek0388@gmail.com>
2020-10-30 16:56:23 +0530

    threads synchro

---
+++ java-basics/src/main/java/com/abhishek/multithreading/synclocks/SyncLocks_1.java
@@ -0,0 +1,39 @@
+package com.abhishek.multithreading.synclocks;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SyncLocks_1 {
+
+    public static void main(String[] args) {
+
+        Counter cc= new Counter();
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        /*IntStream.range(0, 100)
+                .forEach(i -> executor.submit(new Thread(new Runner(cc))));*/
+        for (int i =0; i<=100;i++) {
+            Runner rr= new Runner(cc);
+            executor.submit(rr);
+        }
+        executor.shutdown();
+        System.out.println(cc.count);
+    }
+}
+
+class Runner implements Runnable {
+    Counter counter;
+    public Runner(Counter counter) {
+        this.counter = counter;
+    }
+    @Override
+    public void run() {
+        counter.incrementCount();
+    }
+}
+
+class Counter {
+    int count;
+    public synchronized void incrementCount() {
+        this.count = count+1;
+    }
+}
