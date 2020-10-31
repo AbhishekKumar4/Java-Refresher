7da6a9bc03fbc5fd08e2b3c8ac608666cad78d2b
Abhishek <kumarabhishek0388@gmail.com>
2020-10-31 12:29:53 +0530

    andThen and compose

--- java-basics/src/main/java/com/abhishek/java8/functional/BiFunctionExample_5.java
+++ java-basics/src/main/java/com/abhishek/java8/functional/BiFunctionExample_5.java
@@ -7,11 +7,22 @@ public class BiFunctionExample_5 {
 
     public static void main(String[] args) {
 
-        Function<Integer, Integer> function = a -> a * 2;
-        System.out.println(function.apply(5));
+        Function<Integer, Integer> function = a -> {
+            System.out.println("I am function");
+            return a * 2;
+        };
+        //System.out.println(function.apply(5));
+
+        Function<Integer, Integer> function2 = a -> {
+            System.out.println("I am function 2");
+            return a + 2;
+        };
+
+        //System.out.println(function.andThen(function2).apply(2));
+        System.out.println(function.compose(function2).apply(2));
 
         BiFunction<Integer, Integer, Integer> biFunction = (a, b) -> a * b;
-        System.out.println(biFunction.apply(2,3));
+        //System.out.println(biFunction.apply(2,3));
 
     }
 }
